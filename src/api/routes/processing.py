58ef1d53a73a63c0d5e52fc64779b4a623ca25d8
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-05-27 09:58:47 +0100

    Ui and backend added

---
+++ src/api/routes/processing.py
@@ -0,0 +1,9 @@
+# from src.services import generate_company_domains
+#
+#
+# def start_processing(property_details: str, compose_email_prompt: str):
+#     company_domains: list[str] = generate_company_domains(property_details)
+#     for company_domain in company_domain:
+#         emails = extract_domain()
+#         for email in emails:
+#             compose_email = compose_email(email)
