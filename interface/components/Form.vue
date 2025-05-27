4c3c1ed3b8f18a7723b17b97429983fe6679f4b7
Euvince CAPO CHICHI <danieleuvince20032gmail.com>
2025-05-27 18:21:43 +0100

    Refacto in progress...

--- interface/components/Form.vue
+++ interface/components/Form.vue
@@ -6,24 +6,18 @@
       </h1>
 
       <form class="space-y-6">
-        <div>
-          <label class="block text-base text-gray-300 mb-1 font-semibold">Property Details</label>
-          <textarea
-            rows="5"
+        <TextArea
             v-model="propertyDetails"
-            class="w-full px-4 py-2 border border-gray-700 bg-[#2c2c2e] rounded text-white placeholder-gray-400 focus:outline-none focus:border-white"
-          />
-        </div>
-
-        <div>
-          <label class="block text-base text-gray-300 mb-1 font-semibold">Compose Email Prompt</label>
-          <textarea
-            rows="5"
+            label="Property Details"
+            placeholder="Enter property details..."
+            :rows="5"
+        />
+        <TextArea
             v-model="composeEmailPrompt"
-            class="w-full px-4 py-2 border border-gray-700 bg-[#2c2c2e] rounded text-white placeholder-gray-400 focus:outline-none focus:border-white"
-          />
-        </div>
-
+            label="Compose Email Prompt"
+            placeholder="Enter email prompt..."
+            :rows="5"
+        />
         <div class="flex flex-col sm:flex-row gap-4 pt-6">
           <button
             @click="submitForm"
@@ -74,20 +68,25 @@
 
 
 <script setup lang="ts">
-    const propertyDetails= ref('')
-    const composeEmailPrompt = ref('')
+    const propertyDetails= ref('Property Detailssssssss')
+    const composeEmailPrompt = ref('Email Prompttttttt')
     const drawerOpen = ref(false)
 
     const submitForm = () => {
-      console.log('Starting bot with configuration:', {
+      console.log('Starting process:', {
         propertyDetails: propertyDetails.value,
         composeEmailPrompt: composeEmailPrompt.value
       })
       drawerOpen.value = true
     }
 
-    const clearLog = () => {}
-    const refreshLog= () => {}
+    const refreshLog= () => {
+        console.log("Refresh Logs...")
+    }
+
+    const clearLog = () => {
+        console.log("Clear Logs...")
+    }
 </script>
 
 
---
+++ interface/components/TextArea.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <label class="block text-base text-gray-300 mb-1 font-semibold">{{ label }}</label>
+        <textarea
+            :rows="rows"
+            :placeholder="placeholder"
+            :value="modelValue"
+            class="w-full px-4 py-2 border border-gray-700 bg-[#2c2c2e] rounded text-white placeholder-gray-400 focus:outline-none focus:border-white"
+        />
+    </div>
+</template>
+
+
+<script setup lang="ts">
+    defineProps<{
+        label: string
+        modelValue: string
+        placeholder?: string
+        rows?: number
+    }>()
+</script>
+
+<style>
+</style>
