5c2495dcb59e64b101d3c1ea43a8a7ee803d3960
Euvince CAPO CHICHI <danieleuvince20032gmail.com>
2025-05-28 15:59:57 +0100

    Refacto

--- interface/server/api/logs.ts
+++ interface/server/api/logs.ts
@@ -1,7 +1,11 @@
 export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
   const baseUrl = useRuntimeConfig().public.API_BASE_URL as string
   const url = `${baseUrl}/api/get-log`
-  const data = await $fetch(url)
+  const data = await $fetch(url, {
+    method: 'GET',
+    params: query,
+  })
 
   return data
 })
