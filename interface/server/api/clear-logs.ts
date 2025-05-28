23c5a1e554e198977c59a0a94c0b08c7b2dd83e1
Euvince CAPO CHICHI <danieleuvince20032gmail.com>
2025-05-28 13:43:34 +0100

    Configs

--- interface/server/api/clear-logs.ts
+++ interface/server/api/clear-logs.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const baseUrl = useRuntimeConfig().public.API_BASE_URL as string
-  const url = `${baseUrl}/clear-logs`
+  const url = `${baseUrl}/clear-log`
   const data = await $fetch(url)
 
   return data
