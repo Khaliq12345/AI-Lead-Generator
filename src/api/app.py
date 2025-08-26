83aac9151f9beed3a0f50a595e77588302257f12
Hafiz <abdulmoustapha64@@gmail.com>
2025-08-26 12:51:34 +0100

    Start Scraper Endpoint

--- src/api/app.py
+++ src/api/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.routes.processing import router as processing_router
 from src.api.routes.logs import router as logs_router
+from src.api.routes.scraper import router as scraper_router
 from src.core.config import ENV
 
 app = FastAPI(
@@ -22,6 +23,7 @@ app.add_middleware(
 # Routes
 app.include_router(prefix="/api", router=processing_router, tags=["Processing"])
 app.include_router(prefix="/api", router=logs_router, tags=["Logs"])
+app.include_router(prefix="/api", router=scraper_router, tags=["Scraper"])
 
 
 def start_app():
---
+++ src/api/routes/scraper.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException
+from src.services.scraper_service import run
+import os
+
+router = APIRouter(prefix="", responses={404: {"description": "Not found"}})
+
+
+# Start Scraping
+@router.get("/scrape-link", response_model=str)
+def scrap_link(url: str) -> str:
+    try:
+        current_dir = os.path.dirname(__file__)
+        file_path = os.path.join(current_dir, "..", "..", "services", "rel.json")
+        file_path = os.path.abspath(file_path)
+        if not os.path.isfile(file_path):
+            raise HTTPException(400, detail="Unable to Find Session File rel.json")
+        output_path = run(url)
+        if not output_path:
+            raise HTTPException(400, detail=f"Unable to Scrape the url : {url}")
+        return output_path
+    except Exception as e:
+        raise HTTPException(500, detail=str(e))
