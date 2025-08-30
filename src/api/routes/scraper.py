9e8acdae634359e7421cda470fafe87fa5cf43b3
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2025-08-30 16:23:59 +0100

    Data Extraction Fix

--- src/api/routes/scraper.py
+++ src/api/routes/scraper.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from src.core import config
 from src.services.scraper_service import run
 import os
 
@@ -9,17 +11,9 @@ router = APIRouter(prefix="", responses={404: {"description": "Not found"}})
 @router.get("/scrape-link", response_model=str)
 def scrap_link(url: str):
     try:
-        current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, "..", "..", "services", "rel.json")
-        file_path = os.path.abspath(file_path)
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(config.REL_PATH or ""):
             raise HTTPException(400, detail="Unable to Find Session File rel.json")
-        print("here")
-        output_path = run(url)
-        if not output_path:
-            raise HTTPException(400, detail=f"Unable to Scrape the url : {url}")
-        with open(output_path, "r") as f:
-            content = f.read()
-            return content
+        content = run(url)
+        return JSONResponse(content=content)
     except Exception as e:
         raise HTTPException(500, detail=str(e))
